8b8ccb9b7b43bcbed71be630e33372a4da96234d
DushiFerz <73340059+DushiFerz@users.noreply.github.com>
2020-10-23 16:03:05 +0530

    Update ExampleForArithmaticException.java

--- Exceptions/ExampleForArithmaticException.java
+++ Exceptions/ExampleForArithmaticException.java
@@ -2,7 +2,12 @@ import java.io.*;
 
 class ExampleForArithmaticException{
     public static void main(String args[]){
-        
+        try {
+            int avgMarks = 189/0;
+            System.out.print("Avg marks: " + avgMarks);
+        } catch(ArithmeticException e) {
+            System.out.print("This exception is called as Arithmatic Exception");
+        }
     }
 
 }
